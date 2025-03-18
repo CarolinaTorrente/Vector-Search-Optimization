6ba99072874e7f5c2807ae75689c3e2fac0234c1
Carolina Torrente <121116840+CarolinaTorrente@users.noreply.github.com>
2025-03-18 16:48:47 +0100

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -2,23 +2,10 @@ CHAT_GPT_MAX_IMG_SIZE = 768
 
 PROMPTS = [
     (
-        "You are an advanced OCR model capable of not only reading text but also understanding and organizing it."
-        " When you receive an image of a document, extract the text logically, considering text groupings, such as"
-        " text boxes. If the document contains tables, return them in markdown format, accounting for merged cells."
-        "If there are images within the text, describe their content precisely and explain any relation to other "
-        "images or text. Focus on how images may contribute to understanding the document, such as diagrams or "
-        "explanatory images. Provide all additional text in Spanish."
+        "You are an advanced OCR model capable of not only reading text but also understanding and org..."
     ),
     (
-        "You are the most advanced ORC model, you are capable of not only reading text but also understanding it and "
-        "finding the text agrupations in a file, for example when there is a box of text in a document, you must "
-        "cluster all the text inside that box. You are going to receive a image of a document and you must extract "
-        "the text from it in a logical way. There can be tables in the document, return them in markdown format, "
-        "keep in mind tables can have joined cells. If you see an image inside the text with text or something"
-        " relevant, describe precisely its contents and if there is relation with the rest of the images as there can"
-        " diagrams and the images can explain something, you have to explain what the images try to say. Answer only"
-        " with the extracted text or images. All the text that is not originally in the document that you add "
-        "must be in spanish."
+        "You are the most advanced ORC model, you are capable of not only reading text but also ..."
     ),
 ]
 
